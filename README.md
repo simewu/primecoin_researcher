5cda4dd20e2f1cf3bb26f7c1851cad68b413578b
simewu <swuthier@uccs.edu>
2022-05-24 21:45:52 -0600

    v0.2.0xpm-*

---
+++ README.md
@@ -0,0 +1,102 @@
+Primecoin integration/staging tree
+==================================
+
+http://primecoin.org
+
+Copyright (c) 2013 Primecoin Developers
+
+Copyright (c) 2009-2013 Bitcoin Developers
+
+Copyright (c) 2011-2013 PPCoin Developers
+
+What is Primecoin?
+------------------
+
+Primecoin is an experimental cryptocurrency that introduces the first
+scientific computing proof-of-work to cryptocurrency technology. Primecoin's
+proof-of-work is an innovative design based on searching for prime number
+chains, providing potential scientific value in addition to minting and
+security for the network. Similar to Bitcoin, Primecoin enables instant payments
+to anyone, anywhere in the world. It also uses peer-to-peer technology to 
+operate with no central authority: managing transactions and issuing money are 
+carried out collectively by the network. Primecoin is also the name of the open
+source software which enables the use of this currency.
+
+For more information, as well as an immediately useable, binary version of
+the Primecoin client sofware, see http://primecoin.org.
+
+License
+-------
+
+Primecoin is released under conditional MIT license. See  COPYING` for more
+information.
+
+Development process
+-------------------
+
+Developers work in their own trees, then submit pull requests when they think
+their feature or bug fix is ready.
+
+If it is a simple/trivial/non-controversial change, then one of the Bitcoin
+development team members simply pulls it.
+
+If it is a *more complicated or potentially controversial* change, then the patch
+submitter will be asked to start a discussion (if they haven't already) on the
+ppcoin/primecoin forum (https://talk.peercoin.net).
+
+The patch will be accepted if there is broad consensus that it is a good thing.
+Developers should expect to rework and resubmit patches if the code doesn't
+match the project's coding conventions (see `doc/coding.md`) or are
+controversial.
+
+The `master` branch is regularly built and tested, but is not guaranteed to be
+completely stable. [Tags](https://github.com/primecoin/primecoin/tags) are
+created regularly to indicate new official, stable release versions of
+Primecoin.
+
+Testing
+-------
+
+Testing and code review is the bottleneck for development; we get more pull
+requests than we can review and test. Please be patient and help out, and
+remember this is a security-critical project where any mistake might cost people
+lots of money.
+
+### Automated Testing
+
+Developers are strongly encouraged to write unit tests for new code, and to
+submit new unit tests for old code.
+
+Unit tests for the core code are in `src/test/`. To compile and run them:
+
+    sudo apt-get update
+    sudo apt-get install build-essential
+    sudo apt-get install -y build-essential autoconf libtool pkg-config libboost-all-dev libssl-dev libevent-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev libminiupnpc-dev curl
+    git clone https://github.com/primecoin/primecoin.git
+    cd primecoin
+
+    (In ubuntu 20.04)
+    cd depends; make
+    export BOOST_INCLUDE_PATH=`pwd`/x86_64-pc-linux-gnu/include
+    export BDB_INCLUDE_PATH=`pwd`/x86_64-pc-linux-gnu/include
+    export OPENSSL_INCLUDE_PATH=`pwd`/x86_64-pc-linux-gnu/include
+    export BOOST_LIB_PATH=`pwd`/x86_64-pc-linux-gnu/lib
+    export BOOST_LIB_SUFFIX=-mt
+    cd ..
+    cd src ; make -f makefile.unix USE_UPNP:=
+
+    (For other systems)
+    cd src ; make -f makefile.unix
+
+Unit tests for the GUI code are in `src/qt/test/`. To compile and run them:
+
+    qmake BITCOIN_QT_TEST=1 -o Makefile.test bitcoin-qt.pro
+    make -f Makefile.test
+    ./bitcoin-qt_test
+
+### Manual Quality Assurance (QA) Testing
+
+Large changes should have a test plan, and should be tested by somebody other
+than the developer who wrote the code.
+
+See https://github.com/bitcoin/QA/ for how to create a test plan.
---
+++ src/checkpoints.cpp
@@ -0,0 +1,148 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2013 Primecoin developers
+// Distributed under conditional MIT/X11 software license,
+// see the accompanying file COPYING
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "main.h"
+#include "uint256.h"
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    // How many times we expect transactions after the last checkpoint to
+    // be slower. This number is a compromise, as it can't be accurate for
+    // every system. When reindexing from a fast disk with a slow CPU, it
+    // can be up to 20, while when downloading from a slow network with a
+    // fast multicore CPU, it won't be much higher than 1.
+    static const double fSigcheckVerificationFactor = 5.0;
+
+    struct CCheckpointData {
+        const MapCheckpoints *mapCheckpoints;
+        int64 nTimeLastCheckpoint;
+        int64 nTransactionsLastCheckpoint;
+        double fTransactionsPerDay;
+    };
+
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    static MapCheckpoints mapCheckpoints =
+        boost::assign::map_list_of
+        ( 0, hashGenesisBlockOfficial )
+        ( 7000, uint256("0x8129a538540682e37ffda6a38a21d73b13b00d97c4b865f2976f0e5a873778fc"))
+        ( 70000, uint256("0x89b8dafd07efc96e802594f0b497fb958dfdf1af7cf73f2b46f975ab0c36aa53"))
+        ( 700000, uint256("0xece5513cb4ccc49479f0831b6fea8c05d1716183a1ca968e9acfae0c339d1a32"))
+        ( 1400000, uint256("0xe03d4012d38c30d5224acaadf9e9020a8cf3e9f9037402931dda6224ea2c5515"))
+        ( 2100000, uint256("0x5084a216b157d585cd9c804bad834a6aae1bc12cdc6dd7e2a4a3c2f35b45ebfa"))
+        ( 2800000, uint256("0x108d47a6d15fb901254bf83e37a7b3c7314187b899423d15d7690208888870ce"))
+        ;
+    static const CCheckpointData data = {
+        &mapCheckpoints,
+        1534636137, // * UNIX timestamp of last checkpoint block
+        5621243,   // * total number of transactions between genesis and last checkpoint
+                    //   (the tx=... number in the SetBestChain debug.log lines)
+        3006.0     // * estimated number of transactions per day after checkpoint
+    };
+
+    static MapCheckpoints mapCheckpointsTestnet = 
+        boost::assign::map_list_of
+        ( 0, hashGenesisBlockTestNet )
+        ;
+    static const CCheckpointData dataTestnet = {
+        &mapCheckpointsTestnet,
+        1338180505,
+        16341,
+        300
+    };
+
+    const CCheckpointData &Checkpoints() {
+        if (fTestNet)
+            return dataTestnet;
+        else
+            return data;
+    }
+
+    bool CheckBlock(int nHeight, const uint256& hash)
+    {
+        if (!GetBoolArg("-checkpoints", true))
+            return true;
+
+        const MapCheckpoints& checkpoints = *Checkpoints().mapCheckpoints;
+
+        MapCheckpoints::const_iterator i = checkpoints.find(nHeight);
+        if (i == checkpoints.end()) return true;
+        return hash == i->second;
+    }
+
+    // Guess how far we are in the verification process at the given block index
+    double GuessVerificationProgress(CBlockIndex *pindex) {
+        if (pindex==NULL)
+            return 0.0;
+
+        int64 nNow = time(NULL);
+
+        double fWorkBefore = 0.0; // Amount of work done before pindex
+        double fWorkAfter = 0.0;  // Amount of work left after pindex (estimated)
+        // Work is defined as: 1.0 per transaction before the last checkoint, and
+        // fSigcheckVerificationFactor per transaction after.
+
+        const CCheckpointData &data = Checkpoints();
+
+        if (pindex->nChainTx <= data.nTransactionsLastCheckpoint) {
+            double nCheapBefore = pindex->nChainTx;
+            double nCheapAfter = data.nTransactionsLastCheckpoint - pindex->nChainTx;
+            double nExpensiveAfter = (nNow - data.nTimeLastCheckpoint)/86400.0*data.fTransactionsPerDay;
+            fWorkBefore = nCheapBefore;
+            fWorkAfter = nCheapAfter + nExpensiveAfter*fSigcheckVerificationFactor;
+        } else {
+            double nCheapBefore = data.nTransactionsLastCheckpoint;
+            double nExpensiveBefore = pindex->nChainTx - data.nTransactionsLastCheckpoint;
+            double nExpensiveAfter = (nNow - pindex->nTime)/86400.0*data.fTransactionsPerDay;
+            fWorkBefore = nCheapBefore + nExpensiveBefore*fSigcheckVerificationFactor;
+            fWorkAfter = nExpensiveAfter*fSigcheckVerificationFactor;
+        }
+
+        return fWorkBefore / (fWorkBefore + fWorkAfter);
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        if (!GetBoolArg("-checkpoints", true))
+            return 0;
+
+        const MapCheckpoints& checkpoints = *Checkpoints().mapCheckpoints;
+
+        return checkpoints.rbegin()->first;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        if (!GetBoolArg("-checkpoints", true))
+            return NULL;
+
+        const MapCheckpoints& checkpoints = *Checkpoints().mapCheckpoints;
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, checkpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+        }
+        return NULL;
+    }
+
+    uint256 GetLatestHardenedCheckpoint()
+    {
+        const MapCheckpoints& checkpoints = *Checkpoints().mapCheckpoints;
+        return (checkpoints.rbegin()->second);
+    }
+}
